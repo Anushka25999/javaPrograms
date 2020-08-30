c585c45c1810a830a83c71300dce84eefe4e2b89
anushka <anushka25999@gmail.com>
2020-08-30 17:02:13 +0530

    Updated: EmpWageBuilder.java with UC9

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,8 +2,20 @@ public class EmpWageBuilder {
         public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
 
-        public static int computeEmpWage(String company, int empRatePerHour,
-                                        int numOfWorkingDays, int maxHoursPerMonth) {
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+        public EmpWageBuilder(String company, int empRatePerHour,
+                                    int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void computeEmpWage() {
                 //Variables
                 int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
                 // Computation
@@ -23,13 +35,20 @@ public class EmpWageBuilder {
                         totalEmpHrs += empHrs;
                         System.out.println("Day:" +totalWorkingDays+ " Emp Hr: " +empHrs);
                 }
-                int totalEmpWage = totalEmpHrs * empRatePerHour;
-                System.out.println("Total Emp Wage for Company: " +company+ " is: " +totalEmpWage);
-                return totalEmpWage;
+                totalEmpWage = totalEmpHrs * empRatePerHour;
         }
 
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company: "+company+" is: "+totalEmpWage;
+	}
+
         public static void main(String[] args) {
-                computeEmpWage("Infosys", 20, 20, 100);
-                computeEmpWage("Wipro", 10, 10, 50);
+                EmpWageBuilder infosys = new EmpWageBuilder("Infosys", 20, 20, 100);
+                EmpWageBuilder wipro = new EmpWageBuilder("Wipro", 10, 10, 50);
+		infosys.computeEmpWage();
+		System.out.println(infosys);
+		wipro.computeEmpWage();
+		System.out.println(wipro);
         }
 }

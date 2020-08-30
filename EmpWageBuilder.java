5b66e1bfb24d17202131a8e60709490403708bcc
anushka <anushka25999@gmail.com>
2020-08-30 19:08:56 +0530

    Updated: EmpWageBuilder.java with UC11

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,4 +1,4 @@
-public class EmpWageBuilder {
+public class EmpWageBuilder implements IComputeEmpWage {
         public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
 
@@ -9,21 +9,21 @@ public class EmpWageBuilder {
 		companyEmpWageArray = new CompanyEmpWage[5];
 	}
 
-	private void addCompanyEmpWage(String company, int empRatePerHour,
+	public void addCompanyEmpWage(String company, int empRatePerHour,
 				     int numOfWorkingDays, int maxHoursPerMonth){
 		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour,
 									 numOfWorkingDays, maxHoursPerMonth);
 		numOfCompany++;
 	}
 
-	private void computeEmpWage() {
+	public void computeEmpWage() {
 		for(int i = 0; i<numOfCompany; i++) {
 			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
 			System.out.println(companyEmpWageArray[i]);
 		}
 	}
 
-	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+	public int computeEmpWage(CompanyEmpWage companyEmpWage) {
                 //Variables
                 int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
                 // Computation

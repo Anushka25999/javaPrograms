381eabf257b8521232b7be26e5a781bf891d12d0
anushka <anushka25999@gmail.com>
2020-08-30 15:36:19 +0530

    Updated: EmpWageBuilder.java with UC7

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,7 +5,7 @@ public class EmpWageBuilder {
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		//Variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
@@ -27,5 +27,10 @@ public class EmpWageBuilder {
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " +totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+		computeEmpWage();
 	}
 }

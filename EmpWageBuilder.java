95dc99836457a3d4d697b1fa10949b39e003aeca
anushka <anushka25999@gmail.com>
2020-08-30 09:33:26 +0530

    Updated: EmpWageBuilder.java file

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,4 +1,4 @@
-public class EmpWageBuilder.java {
+public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 	}
